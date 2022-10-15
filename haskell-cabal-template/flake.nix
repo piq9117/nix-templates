ac20c6a415c3cbc65794f2b6750e2cbadc451951
piq9117 <piq9117@gmail.com>
2022-10-15 00:47:01 -0400

    template: haskell template overlay
    - provide blank overlay and packages field.

--- haskell-cabal-template/flake.nix
+++ haskell-cabal-template/flake.nix
@@ -7,9 +7,14 @@
     let
       supportedSystems = [ "x86_64-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin" ];
       forAllSystems = f: nixpkgs.lib.genAttrs supportedSystems (system: f system);
-      nixpkgsFor = forAllSystems (system: import nixpkgs {inherit system;});
+      nixpkgsFor = forAllSystems (system: import nixpkgs {
+        inherit system;
+        overlays = [self.overlay];
+      });
 
     in {
+      overlay = final: prev: {};
+      packages = forAllSystems (system: {});
       devShells = forAllSystems(system:
         let pkgs = nixpkgsFor.${system};
         in {
