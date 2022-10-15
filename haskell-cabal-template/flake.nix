b8574aecd7eaaec6f72916e7f77fabbd1b34fa45
piq9117 <piq9117@gmail.com>
2022-10-15 00:37:07 -0400

    template: haskelll template pinned
    - pinned to nixos-22.05

--- haskell-cabal-template/flake.nix
+++ haskell-cabal-template/flake.nix
@@ -1,6 +1,8 @@
 {
   description = "A very basic flake";
 
+  inputs.nixpkgs.url = "nixpkgs/nixos-22.05";
+
   outputs = { self, nixpkgs }:
     let
       supportedSystems = [ "x86_64-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin" ];
