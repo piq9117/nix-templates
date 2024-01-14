71712175a4426593a153f2799f0bcc781b3c4d8b
piq9117 <piq9117@gmail.com>
2024-01-14 17:33:13 -0500

    bump go template

--- go-template/flake.nix
+++ go-template/flake.nix
@@ -1,7 +1,7 @@
 {
   description = "Basic go template";
 
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/22.11";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/23.11";
 
   outputs = { self, nixpkgs }:
     let
