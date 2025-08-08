71d9c2fc3d2b360035128552287b329d4ad1e4f3
piq9117 <piq9117@gmail.com>
2025-08-08 16:09:43 -0400

    haskell stack template

---
+++ haskell-stack-template/flake.nix
@@ -0,0 +1,53 @@
+{
+  inputs.nixpkgs.url = github:NixOS/nixpkgs/nixos-unstable;
+  outputs = {self, nixpkgs}: 
+    let
+      forAllSystems = nixpkgs.lib.genAttrs nixpkgs.lib.systems.flakeExposed;
+      nixpkgsFor = forAllSystems(system: import nixpkgs {
+        inherit system;
+        overlays = [ self.overlays ];
+      });
+    in {
+      overlays = final: prev: {
+        hsPkgs = prev.haskell.packages.ghc9102.override {
+          overrides = hfinal: hprev: {
+          };
+        }; 
+      };
+      devShells = forAllSystems(system: 
+        let
+          pkgs = nixpkgsFor.${system};
+          stack-wrapped = pkgs.symlinkJoin {
+            name = "stack";
+            paths = [ pkgs.stack ];
+            buildInputs = [ pkgs.makeWrapper ];
+            postBuild = ''
+              wrapProgram $out/bin/stack \
+                --add-flags "\
+                  --no-nix \
+                  --system-ghc \
+                  --no-install-ghc \
+                "
+            '';
+          };
+
+          devTools = with pkgs; [
+                zlib
+                hsPkgs.ghc
+                hsPkgs.haskell-language-server
+                hsPkgs.hpack
+                stack-wrapped
+          ];
+        in {
+          default = pkgs.mkShell {
+            buildInputs = devTools;
+            withHoogle = true;
+            shellHook = ''
+              export PS1='[$PWD]\n❄ '
+              export NIX_GHC="${pkgs.hsPkgs.ghc}/bin/ghc"
+            '';
+            LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath devTools;
+          };
+        });
+    };
+}
