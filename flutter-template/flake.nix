fbd357405c8981cc30cf5ad20e3a524c3942411b
piq9117 <piq9117@gmail.com>
2026-01-12 15:18:55 -0500

    flutter template

---
+++ flutter-template/flake.nix
@@ -0,0 +1,43 @@
+{
+  description = "Flutter template";
+
+  inputs = {
+    nixpkgs.url = github:NixOS/nixpkgs/nixpkgs-unstable;
+  };
+
+  outputs = {self, nixpkgs}: 
+  let
+    forAllSystems = nixpkgs.lib.genAttrs nixpkgs.lib.systems.flakeExposed;
+    nixpkgsFor = forAllSystems(system: import nixpkgs {
+      inherit system;
+      overlays = [self.overlays];
+      config = {
+        android_sdk.accept_license = true;
+        allowUnfree = true;
+      };
+    });
+  
+  in {
+    overlays = final: prev: {};
+    devShells = forAllSystems(system: 
+      let
+        pkgs = nixpkgsFor.${system};
+        androidComposition = pkgs.androidenv.composeAndroidPackages {
+          platformVersions = ["34" "28"];
+          abiVersions = ["armeabi-v7a" "arm64-v8a"];
+        };
+        androidSdk = androidComposition.androidsdk;
+      in {
+        default = pkgs.mkShell rec {
+          ANDROID_SDK_ROOT = "${androidSdk}/libexec/android-sdk";
+          buildInputs = with pkgs; [
+            flutter
+            androidSdk
+            jdk17
+            nixpkgs-fmt
+            treefmt
+          ];
+        };
+      });
+  };
+}
